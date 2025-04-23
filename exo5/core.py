ea7291730f5e75752ab3ea3975c85dd4101a27cf
Ant1 <an.giusti91@gmail.com>
2025-04-23 14:51:02 +0200

    - start exo5

---
+++ exo5/core.py
@@ -0,0 +1,51 @@
+"""
+===============================================================================
+fileName: exo5.core
+scripter: angiu
+creation date: 23/04/2025
+description: 
+===============================================================================
+"""
+# ==== native ==== #
+import os
+
+# ==== third ==== #
+
+# ==== local ===== #
+from exercices.library.fileLib import getCSVData
+from exercices.library.fileLib import getFileRecursively
+
+# ==== global ==== #
+PACKAGE_REPO = os.sep.join(__file__.split(os.sep)[:-2])
+FILE_NAME = 'capteurs.csv'
+EXTENSION = '.csv'
+
+
+def getAvailableCsv():
+    """
+    Recursively traverses a directory to return a dictionary of available CSV files.
+
+    This function uses the `getFileRecursively` function to gather all files from the directory
+    specified by `PACKAGE_REPO`. It then filters the files based on their extension, keeping only
+    those that match the extension defined in `EXTENSION`. The returned dictionary maps the short
+    name of each file (without its extension) to its full path.
+
+    :return: names of the CSV and related fullPath
+    :rtype: dict
+    """
+    files = getFileRecursively(PACKAGE_REPO)
+
+    data = {}
+    for item in files:
+        extension = os.path.splitext(item)[-1]
+        if extension != EXTENSION:
+            continue
+
+        shortName = os.path.splitext(os.path.split(item)[-1])[0]
+        data[shortName] = item
+
+    return data
+
+
+def getDataFromCSVPath(chosenCSV):
+    data = getCSVData(filePath=chosenCSV)
---
+++ exo5/ui.py
@@ -0,0 +1,52 @@
+"""
+===============================================================================
+fileName: exo5.ui
+scripter: angiu
+creation date: 23/04/2025
+description: 
+===============================================================================
+"""
+# ==== native ==== #
+
+# ==== third ==== #
+from PySide2 import QtWidgets
+from PySide2 import QtCore
+
+# ==== local ===== #
+
+# ==== global ==== #
+
+
+class CsvViewerUi(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(CsvViewerUi, self).__init__(parent)
+        self.uiName = __class__.__name__.split('Ui')[0]
+
+        self.uiWidth = 300
+        self.uiMenus = {}
+        self.setupUi()
+
+    def setupUi(self):
+        self.setWindowTitle(self.uiName)
+        self.setMinimumWidth(self.uiWidth + 45)
+        self.setWindowFlags(QtCore.Qt.Dialog | QtCore.Qt.WindowTitleHint | QtCore.Qt.WindowCloseButtonHint)
+
+        self.mainLayout = QtWidgets.QVBoxLayout(self)
+
+        csvSelectLayout = QtWidgets.QHBoxLayout()
+        csvSelectLabel = QtWidgets.QLabel('available files :')
+        csvSelectLabel.setMinimumWidth(self.uiWidth / 3)
+        csvSelectLabel.setMinimumHeight(30)
+        csvSelectLabel.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        csvSelectLayout.addWidget(csvSelectLabel)
+        self.CSVSelecter = QtWidgets.QComboBox()
+        self.CSVSelecter.setMinimumWidth((self.uiWidth / 3) * 2)
+        self.CSVSelecter.setMinimumHeight(30)
+        self.CSVSelecter.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        csvSelectLayout.addWidget(self.CSVSelecter)
+        self.mainLayout.addLayout(csvSelectLayout)
+
+        self.treeView = QtWidgets.QTreeView()
+        self.treeView.setSortingEnabled(True)  # Enable sorting by column header
+        self.treeView.setAlternatingRowColors(True)  # Optional: for better row color alternation
+        self.mainLayout.addWidget(self.treeView)
